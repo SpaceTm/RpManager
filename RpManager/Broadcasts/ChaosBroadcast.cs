9776f3639716513db8c737dc745eb13d6b4853a2
TemmieGamerGuy <76136819+TemmieGamerGuy@users.noreply.github.com>
2021-09-09 15:46:28 +0100

    fixed

--- RpManager/Broadcasts/ChaosBroadcast.cs
+++ RpManager/Broadcasts/ChaosBroadcast.cs
@@ -11,25 +11,10 @@ namespace RpManager.Broadcasts
     {
         public void OnChaosSpawning(SpawningEventArgs ev)
         {
-                if (ev.Player.Role == RoleType.ChaosConscript)
-                {
-                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
-                }
-
-                if (ev.Player.Role == RoleType.ChaosMarauder)
-                {
-                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
-                }
-
-                if (ev.Player.Role == RoleType.ChaosRepressor)
-                {
-                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
-                }
-
-                if (ev.Player.Role == RoleType.ChaosRifleman)
-                {
-                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
-                {
+            if (ev.Player.Team == Team.CHI)
+            {
+                ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
+            }
         }
     }
 }
