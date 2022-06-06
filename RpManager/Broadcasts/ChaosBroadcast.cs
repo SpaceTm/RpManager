2abde8146b04777600cd0e0e327b212563375a6f
R Z <richardzemko08@gmail.com>
2022-06-06 19:33:27 +0200

    Update to EXILED 5.2.1

---
+++ RpManager/Broadcasts/ChaosBroadcast.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.Events.EventArgs;
+
+namespace RpManager.Broadcasts
+{
+    class ChaosBroadcast
+    {
+        public void OnChaosSpawning(SpawningEventArgs ev)
+        {
+            if (ev.Player.Team == Team.CHI)
+            {
+                if (ev.Player.Role == RoleType.ChaosConscript)
+                {
+                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
+                }
+
+                if (ev.Player.Role == RoleType.ChaosMarauder)
+                {
+                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
+                }
+
+                if (ev.Player.Role == RoleType.ChaosRepressor)
+                {
+                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
+                }
+
+                if (ev.Player.Role == RoleType.ChaosRifleman)
+                {
+                    ev.Player.Broadcast(10, Plugin.Singleton.Config.ChaosBC);
+                }
+            }
+        }
+    }
+}
